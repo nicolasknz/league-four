9cc3fe65d10b08d25e4f2d5a46d721f6f424bafe
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2020-06-24 09:41:40 -0300

    versão funcional com bugs corrigidos

--- script.js
+++ script.js
@@ -11,108 +11,110 @@ function table() {
 }
 table();
 
-const black = 'black';
-const red = 'red';
-let player = black;
-const jogador = document.getElementById('player');
-jogador.textContent = player;
+function play() {
 
-function switchPlayer() {
-   if (player === 'black') {
-      player = 'red'
-      jogador.textContent = player;
-   } else if (player === 'red') {
-      player = 'black'
-      jogador.textContent = player;
-   }
-}
+   const black = 'black';
+   const red = 'red';
+   let player = black;
+   const jogador = document.getElementById('player');
+   jogador.textContent = player;
 
-// Retorna ficha vermelha se receber 'red' ou preta se receber 'black'
-function createChip(nextPlayer) {
-   let newChip = document.createElement('div');
-   if (nextPlayer === 'red') {
-      newChip.classList.add('red');
-   } else if (nextPlayer === 'black') {
-      newChip.classList.add('black');
-   }
-   return newChip;
-}
-
-// Retorna a quantidade de chips numa determinada coluna
-function columnChipCount(columnSelected) {
-   let chipsCount = columnSelected.getElementsByTagName('*').length;
-   return chipsCount;
-}
-
-// Verifica  a coluna em que o mouse está ao clicar, se ainda houver espaço ira alterar a variavel hover para true, se não houver, altera para false
-let hover;
-let columns = document.getElementsByClassName('column');
-for (column of columns) {
-   column.addEventListener('click', function () {
-      if (columnChipCount(this) < 6) {
-         this.appendChild(createChip(player));
-         switchPlayer();
-      } else {
-         return false;
+   function switchPlayer() {
+      if (player === 'black') {
+         player = 'red'
+         jogador.textContent = player;
+      } else if (player === 'red') {
+         player = 'black'
+         jogador.textContent = player;
       }
-   });
-}
-
-// fim de jogo: div popup com mensagem
-// RECEBE endGame
-function telaFinal(result) {
-   // define variáveis
-   let finalMsg = '';
-   let msgColor = '';
-   switch (result) {
-      // de acordo com o resultado recebido de endGame(), seta mensagem e cor do texto
-      case 'player1':
-         finalMsg = 'Player 1 é o vencedor!!';
-         msgColor = 'blue';
-         break;
-      case 'player2':
-         finalMsg = 'Player 2 é o vencedor!!';
-         msgColor = 'red';
-         break;
-      default:
-         finalMsg = 'Ninguém venceu - empate!!';
-         msgColor = 'green';
-         break;
    }
-   // cria os elementos da página que irão apresentar o resultado do jogo
-   let target = document.getElementById('fimDeJogo');
-   let resultado = document.createElement('div');
-   resultado.style.color = msgColor;
-   let p = document.createElement('p');
-   let txt = document.createTextNode(finalMsg);
-   p.appendChild(txt);
-   resultado.appendChild(p);
-   target.appendChild(resultado);
-   // altera o display para tornar o resultado visível
-   target.style.display = 'flex';
+
+   // Retorna ficha vermelha se receber 'red' ou preta se receber 'black'
+   function createChip(nextPlayer) {
+      let newChip = document.createElement('div');
+      if (nextPlayer === 'red') {
+         newChip.classList.add('red');
+      } else if (nextPlayer === 'black') {
+         newChip.classList.add('black');
+      }
+      return newChip;
+   }
+
+   // Retorna a quantidade de chips numa determinada coluna
+   function columnChipCount(columnSelected) {
+      let chipsCount = columnSelected.getElementsByTagName('*').length;
+      return chipsCount;
+   }
+
+   // Verifica  a coluna em que o mouse está ao clicar, se ainda houver espaço ira alterar a variavel hover para true, se não houver, altera para false
+   let hover;
+   let columns = document.getElementsByClassName('column');
+   for (column of columns) {
+      column.addEventListener('click', function () {
+         if (columnChipCount(this) < 6) {
+            this.appendChild(createChip(player));
+            switchPlayer();
+         } else {
+            return false;
+         }
+      });
+   }
+
+   // fim de jogo: div popup com mensagem
+   // RECEBE endGame
+   function telaFinal(result) {
+      // define variáveis
+      let finalMsg = '';
+      let msgColor = '';
+      switch (result) {
+         // de acordo com o resultado recebido de endGame(), seta mensagem e cor do texto
+         case 'player1':
+            finalMsg = 'Player 1 é o vencedor!!';
+            msgColor = 'blue';
+            break;
+         case 'player2':
+            finalMsg = 'Player 2 é o vencedor!!';
+            msgColor = 'red';
+            break;
+         default:
+            finalMsg = 'Ninguém venceu - empate!!';
+            msgColor = 'green';
+            break;
+      }
+      // cria os elementos da página que irão apresentar o resultado do jogo
+      let target = document.getElementById('fimDeJogo');
+      let resultado = document.createElement('div');
+      resultado.style.color = msgColor;
+      let p = document.createElement('p');
+      let txt = document.createTextNode(finalMsg);
+      p.appendChild(txt);
+      resultado.appendChild(p);
+      target.appendChild(resultado);
+      // altera o display para tornar o resultado visível
+      target.style.display = 'flex';
+   }
+
+
+   const testaVitoria = function () {
+      let resultado = 'empate';
+
+      // TODO
+      // // condição de vitória: 4 em linha (h, v, d para cima, d para baixo)
+      //    // return 'vitória: Player 1' / 'vitória Player 2' / 'Empate'
+      // }
+      // // h -> for(cada array){arrayX[y] === arrayX+1[y] arrX+2..}
+      // // fim do jogo: vitória player 1 - vitória player 2
+      // // empate (acabaram espaços e não atingiu condição de vitória)
+      // 
+
+      telafinal(resultado);
+   }
 }
-
-
-const testaVitoria = function () {
-   let resultado = 'empate';
-
-   // TODO
-   // // condição de vitória: 4 em linha (h, v, d para cima, d para baixo)
-   //    // return 'vitória: Player 1' / 'vitória Player 2' / 'Empate'
-   // }
-   // // h -> for(cada array){arrayX[y] === arrayX+1[y] arrX+2..}
-   // // fim do jogo: vitória player 1 - vitória player 2
-   // // empate (acabaram espaços e não atingiu condição de vitória)
-   // 
-
-   telafinal(resultado);
-}
+play();
 
 const restart = document.getElementById('restart');
 restart.onclick = function () {
-   let divs = main.children;
-   for (div of divs){
-      div.remove();
-   }
+   main.innerHTML = '';
    table();
+   play();
 }
